a6a024f3c979e738724ae2252caab57525d7e4d4
张仲夏(Tony Zhang1) <@tpaicdom.com>
2017-12-29 18:03:52 +0800

    Initial commit

---
+++ src/main/java/DemoServlet.java
@@ -0,0 +1,24 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zhangzx3 on 2017/12/29.
+ */
+@WebServlet("/doDemo")
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("http://www.baidu.com");
+    }
+}
